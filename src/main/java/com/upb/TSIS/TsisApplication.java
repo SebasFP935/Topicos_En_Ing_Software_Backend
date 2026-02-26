4f8f9a5d56b76ca59d82829601ef6d0c5bf47586
XxSebasFxX <sebastianfiorilo@gmail.com>
2026-02-26 08:35:19 -0400

    Baxkend Xreado xon dependenxias, (revisar si falta alguna para el proyexto)

---
+++ src/main/java/com/upb/TSIS/TsisApplication.java
@@ -0,0 +1,13 @@
+package com.upb.TSIS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upb/TSIS/TsisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upb.TSIS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
